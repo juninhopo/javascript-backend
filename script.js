39b45cabeb37a85e95e73d6572191a4f5d3fa9ae
juninhopo <kelgaxd@gmail.com>
2022-02-06 16:45:55 -0300

    update

--- script.js
+++ script.js
@@ -21,4 +21,15 @@
 
 // console.log(area);
 
-const 
+const forma = 'triangulo';
+const altura = 5;
+const comprimento = 7;
+let area = 0;
+
+if (forma === 'quadrado') {
+    area = altura * comprimento;
+} else {
+    area = ((altura * comprimento) / 2)
+}
+
+console.log(area);
