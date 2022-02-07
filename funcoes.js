72d1d4500220ee9cce00066c4c85f2340f2d1528
juninhopo <kelgaxd@gmail.com>
2022-02-07 01:04:04 -0300

    update funcoes

---
+++ funcoes.js
@@ -0,0 +1,30 @@
+// let x = '';
+// console.log(x)
+// x = 'oi';
+
+// DECLARACAO DE FUNCAO
+
+// 1) Declara a funcao
+
+const texto = 'oieeeee'
+
+function imprimeTexto(texto) {
+    console.log(texto);
+}
+
+
+// 2) executa a funcao (1 ou + vezes)
+
+// imprimeTexto('oie');
+// imprimeTexto(texto);
+
+imprimeTexto(soma());
+
+// tres formas de excrever funcoes 
+
+function soma() {
+    return 2 + 2;
+}
+
+console.log(soma())
+
