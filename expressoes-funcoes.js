5638265287e63661a4dd55093e51f9b4ab1ee2a5
juninhopo <kelgaxd@gmail.com>
2022-02-07 01:32:28 -0300

    update

---
+++ expressoes-funcoes.js
@@ -0,0 +1,22 @@
+function minhaFuncao(param) {
+    //bloco de codigo
+}
+
+// minhaFuncao('minha funcao');
+
+//expressao de funcao 
+
+// const soma = function(num1, num2) {return num1 + num2;}
+// console.log(soma(1, 2));
+
+// diferenca principal ?????????????????
+
+console.log(apresentar())
+function apresentar() {
+    return 'ola';
+}
+
+console.log(soma(1,1))
+const soma = function(num1, num2) {return num1 + num2;}
+
+// funcoes e var sao listadas no topo do codigo, o js ele lista todas as var la no topo do codigo, entao ele ve primeiro elas, por isso o segundo metodo nao funciona
