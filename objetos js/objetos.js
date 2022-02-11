496709574ba4d1f373751fc47a23301bc357cd4a
juninhopo <kelgaxd@gmail.com>
2022-02-10 23:07:09 -0300

    update

---
+++ objetos js/objetos.js
@@ -0,0 +1,6 @@
+const cliente = {
+    nome:"Andre",
+    idade:36,
+    cpf:"12345553455",
+    email:"andeisr1@gmail.com"
+}
---
+++ objetos js/print-infos.js
@@ -0,0 +1,10 @@
+const cliente = {
+    nome:'Andre',
+    idade:36,
+    cpf:'12345553455',
+    email:'andeisr1@gmail.com'
+}
+
+console.log(`Meu nome e ${cliente.nome} e tenho ${cliente.idade}`)
+
+console.log(cliente.cpf.substring(0,3))
